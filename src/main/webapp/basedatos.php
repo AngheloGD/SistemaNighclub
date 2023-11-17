5d639da53fcded9ff717419d408b802f7c4ec4cf
HP <HP@DESKTOP-B2OQUTA>
2023-11-16 23:34:36 -0500

    BaseDatos

--- src/main/webapp/basedatos.php
+++ src/main/webapp/basedatos.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html>
 <?php
 $conexion = mysqli_connect('localhost', 'root', '', 'nightclubb');
 ?>
@@ -11,90 +10,90 @@ $conexion = mysqli_connect('localhost', 'root', '', 'nightclubb');
         <link href="css/Reporte.css" rel="stylesheet" type="text/css">
     </head>
     <body>
-        <table>
-            <thead>
-                <tr>
-                    <th>Codigo</th>
-                    <th>Dia de la Cita</th>
-                    <th>Nombre de la Trabajadora</th>
-                    <th>Tipo de Servicio</th>
-                    <th>Cliente</th>
-
-                </tr>
-            </thead>
-            <?php
-            $sql = "SELECT * from cita";
-            $result = mysqli_query($conexion, $sql);
-
-            while ($mostrar = mysqli_fetch_array($result)) {
-                ?>
-                <tbody>
-                    <tr>
-                        <td><?php echo $mostrar['codicita'] ?></td>
-                        <td><?php echo $mostrar['diacita'] ?></td>
-                        <td><?php echo $mostrar['emplcita'] ?></td>
-                        <td><?php echo $mostrar['servcita'] ?></td>
-                        <td><?php echo $mostrar['codiUsua'] ?></td>
-
-                    </tr>
-                </tbody>
-                <?php
-            }
+        <?php
+        // Verificar si el token está almacenado en la cookie
+        if (!isset($_COOKIE['token'])) {
+            // Si no hay token, muestra un mensaje de error y no carga los datos ni genera el informe
+            echo '<div style="color: red; font-size: 18px; margin: 20px;">Error: Inicia sesión para acceder a esta página.</div>';
+        } else {
+            // Token presente, mostrar los datos y el botón para generar el informe
             ?>
-        </table>
-        <button type="button" id="generar-reporte" onclick="generarReporte()">GENERAR REPORTE</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Codigo</th>
+                        <th>Dia de la Cita</th>
+                        <th>Nombre de la Trabajadora</th>
+                        <th>Tipo de Servicio</th>
+                        <th>Cliente</th>
+                    </tr>
+                </thead>
+                <?php
+                $sql = "SELECT * from cita";
+                $result = mysqli_query($conexion, $sql);
 
-        <div id="footer">
-            Fecha y hora de generación: <span id="fecha-hora"></span>
-        </div>
-
-        <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.13/jspdf.plugin.autotable.js"></script>
-        <script>
-            function generarReporte() {
-                console.log("Generando reporte PDF...");
-
-                // Tu código para generar el reporte PDF aquí
-
-                // Ejemplo de generación de reporte PDF
-                var pdf = new jsPDF();
-                pdf.autoTable({html: 'table'});
-                pdf.save("reporte.pdf");
-            }
-
-            // Definición de la variable parametrotoken
-            const PARAMETROTOKEN = "lafedelcuto";
-
-            $.getJSON("llamarToken", PARAMETROTOKEN, function (data) {
-                const token = getCookie("token");
-                console.log("Token del Servidor: " + data.resultado);
-                console.log(token);
-
-                if (token !== data.resultado) {
-                    alert("Error: El token no coincide");
-                    return;
+                while ($mostrar = mysqli_fetch_array($result)) {
+                    ?>
+                    <tbody>
+                        <tr>
+                            <td><?php echo $mostrar['codicita'] ?></td>
+                            <td><?php echo $mostrar['diacita'] ?></td>
+                            <td><?php echo $mostrar['emplcita'] ?></td>
+                            <td><?php echo $mostrar['servcita'] ?></td>
+                            <td><?php echo $mostrar['codiUsua'] ?></td>
+                        </tr>
+                    </tbody>
+                    <?php
                 }
+                ?>
+            </table>
+            <button type="button" id="generar-reporte" onclick="generarReporte()">GENERAR REPORTE</button>
 
+            <div id="footer">
+                Fecha y hora de generación: <span id="fecha-hora"></span>
+            </div>
+
+            <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.4/jspdf.min.js"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.13/jspdf.plugin.autotable.js"></script>
+            <script>
                 document.getElementById("generar-reporte").addEventListener("click", function () {
                     generarReporte();
                 });
-            });
 
-            function getCookie(name) {
-                const nameEQ = name + "=";
-                const cookies = document.cookie.split(';');
-                for (let i = 0; i < cookies.length; i++) {
-                    let cookie = cookies[i];
-                    while (cookie.charAt(0) === ' ') {
-                        cookie = cookie.substring(1, cookie.length);
+                function generarReporte() {
+                    // Verificar si el token está presente antes de generar el informe
+                    if (!getCookie('token')) {
+                        alert('Error: Inicia sesión para generar el informe.');
+                        return;
                     }
-                    if (cookie.indexOf(nameEQ) === 0) {
-                        return cookie.substring(nameEQ.length, cookie.length);
-                    }
+
+                    console.log("Generando reporte PDF...");
+
+                    // Tu código para generar el reporte PDF aquíi
+                    var pdf = new jsPDF();
+                    pdf.autoTable({html: 'table'});
+                    pdf.save("reporte.pdf");
                 }
-                return null;
-            }
-        </script>
+
+                function getCookie(name) {
+                    const nameEQ = name + "=";
+                    const cookies = document.cookie.split(';');
+                    for (let i = 0; i < cookies.length; i++) {
+                        let cookie = cookies[i];
+                        while (cookie.charAt(0) === ' ') {
+                            cookie = cookie.substring(1, cookie.length);
+                        }
+                        if (cookie.indexOf(nameEQ) === 0) {
+                            return cookie.substring(nameEQ.length, cookie.length);
+                        }
+                    }
+                    return null;
+                }
+            </script>
+            <?php
+        }
+        ?>
     </body>
 </html>
+
